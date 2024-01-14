2244123343008da27ec6efbe5c9f46f4d2859753
thohidul.islam <thohidul.islam@trainingmug.com>
2024-01-14 15:14:07 +0600

    add all

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const io = new Server(httpServer, {
 });
 
 const uri =
-  "mongodb+srv://radiationcorporation2:DD6OVn277Mm9KvLj@cluster0.no7secj.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
+  "mongodb+srv://radiationcorporation2:z4HpUfzxQx5jLjZj@cluster0.tpz6mmk.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,20 +27,18 @@ const client = new MongoClient(uri, {
 app.use(cors());
 app.use(express.json());
 
-// io.on("connection", (socket) => {
-//   socket.on("message", (data) => {
-//     console.log(data);
-//   });
-// });
-
 async function run() {
   try {
-    const ticketCollection = client.db("TicketBooking").collection("Ticket");
-    const managerCollection = client.db("TicketBooking").collection("trip");
-    const driverCollection = client.db("TicketBooking").collection("trip");
-    const userCollection = client.db("TicketBooking").collection("users");
-    const adminCollection = client.db("TicketBooking").collection("admin");
-    const messageCollection = client.db("TicketBooking").collection("message");
+    const orderCollection = client.db("BikironStore").collection("Orders");
+    const cartCollection = client.db("BikironStore").collection("cart");
+    const libraryCollection = client.db("BikironStore").collection("library");
+    const bookCollection = client.db("BikironStore").collection("allBooks");
+    const packageCollection = client.db("BikironStore").collection("package");
+    const userCollection = client.db("BikironStore").collection("users");
+    const memberCollection = client.db("BikironStore").collection("members");
+    const allPackages = require("./data/AllPackages.json");
+    const singlePackages = require("./data/SinglePackage.json");
+
     io.on("connection", (socket) => {
       // Listen for incoming messages from a client
       socket.on("message", async (data) => {
@@ -64,26 +62,149 @@ async function run() {
       });
     });
 
-    // io.on("connection", (socket) => {
-    //   socket.on("message", async (data) => {
-    //     try {
-    //       // Save the message to MongoDB
-    //       await messageCollection.insertOne({
-    //         text: data.text,
-    //         sender: data.sender,
-    //       });
+    // api for loading all courses
+    app.get("/allPackages", (req, res) => {
+      res.send(allPackages);
+    });
+    // api for loading all courses
+    app.get("/singlePackages", (req, res) => {
+      res.send(singlePackages);
+    });
 
-    //       // Broadcast the message to all connected clients
-    //       io.emit("message", { text: data.text, sender: data.sender });
-    //     } catch (error) {
-    //       console.error(error);
-    //     }
-    //   });
+    app.put("/addFollow", async (req, res) => {
+      try {
+        const userEmail = req.body.email;
+        const videoId = req.body.id;
 
-    //   socket.on("disconnect", () => {
-    //     console.log("A user disconnected");
-    //   });
-    // });
+        // Assuming videoCollection is a MongoDB collection instance
+        const video = await libraryCollection.findOne(
+          { _id: new ObjectId(videoId) },
+          { sort: { _id: 1 } } // Sort by _id in descending order (newest first)
+        );
+
+        if (video) {
+          // Check if userEmail is not already in the followers array
+          if (!video.followers.includes(userEmail)) {
+            // Add userEmail to the followers array
+            video.followers.push(userEmail);
+
+            // Update the document in the collection
+            await libraryCollection.updateOne(
+              { _id: new ObjectId(videoId) },
+              { $set: { followers: video.followers } }
+            );
+
+            res.json({ success: true, message: "User added to followers" });
+          } else {
+            res.json({ success: false, message: "User is already a follower" });
+          }
+        } else {
+          res.json({ success: false, message: "Video not found" });
+        }
+      } catch (error) {
+        console.error(error);
+        res
+          .status(500)
+          .json({ success: false, message: "Internal server error" });
+      }
+    });
+
+    // get method for finding the specific ticket for the user
+    app.get("/library", async (req, res) => {
+      const query = {};
+      const cursor = libraryCollection.find(query);
+      const message = await cursor.toArray();
+      res.send(message);
+    });
+    app.get("/allBooks", async (req, res) => {
+      const query = {};
+      const cursor = bookCollection.find(query);
+      const message = await cursor.toArray();
+      res.send(message);
+    });
+    app.get("/allBooksPackage", async (req, res) => {
+      const query = {};
+      const cursor = packageCollection.find(query);
+      const message = await cursor.toArray();
+      res.send(message);
+    });
+    app.get("/cart", async (req, res) => {
+      const query = {};
+      const cursor = cartCollection.find(query);
+      const message = await cursor.toArray();
+      res.send(message);
+    });
+
+    app.get("/member", async (req, res) => {
+      const query = {};
+      const cursor = memberCollection.find(query);
+      const message = await cursor.toArray();
+      res.send(message);
+    });
+
+    app.delete("/deleteBook/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.delete("/deleteBookCart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.delete("/deletePackage/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await packageCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+    // post for users method
+    app.post("/addOrder", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    app.post("/addUsers", async (req, res) => {
+      const order = req.body;
+      const result = await userCollection.insertOne(order);
+      res.send(result);
+    });
+    app.post("/addMember", async (req, res) => {
+      const order = req.body;
+      const result = await memberCollection.insertOne(order);
+      res.send(result);
+    });
+    app.post("/addCart", async (req, res) => {
+      const order = req.body;
+      const result = await cartCollection.insertOne(order);
+      res.send(result);
+    });
+    app.post("/addBook", async (req, res) => {
+      const order = req.body;
+      const result = await bookCollection.insertOne(order);
+      res.send(result);
+    });
+    app.post("/addPackage", async (req, res) => {
+      const order = req.body;
+      const result = await packageCollection.insertOne(order);
+      res.send(result);
+    });
+    // post for users method
+    app.post("/createLibrary", async (req, res) => {
+      const order = req.body;
+      const result = await libraryCollection.insertOne(order);
+      res.send(result);
+    });
 
     // get method for finding the specific ticket for the user
     app.get("/message", async (req, res) => {
@@ -92,188 +213,13 @@ async function run() {
       const message = await cursor.toArray();
       res.send(message);
     });
-
-    // get method for finding the specific ticket for the user
-    app.get("/ticket", async (req, res) => {
-      const query = {};
-      const cursor = ticketCollection.find(query);
-      const ticket = await cursor.toArray();
-      res.send(ticket);
-    });
-
-    // find details with id information
-    app.get("/ticket/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await ticketCollection.findOne(query);
-      console.log(result);
-      res.send(result);
-    });
-    // post for add confirm ticket
-    app.post("/addTicket", async (req, res) => {
-      const review = req.body;
-      console.log(review);
-      const result = await ticketCollection.insertOne(review);
-
-      res.send(result);
-    });
-
-    app.post("/addMessage", async (req, res) => {
-      const review = req.body;
-      const result = await messageCollection.insertOne(review);
-
-      res.send(result);
-    });
-
-    app.get("/validateUserRole", async (req, res) => {
-      const userEmail = req.query.email;
-
-      try {
-        // Find the user by email in your MongoDB user collection
-        const user = await userCollection.findOne({ email: userEmail });
-
-        if (user) {
-          res.json({ userRole: user.userRole });
-        } else {
-          res.status(404).json({ message: "User not found" });
-        }
-      } catch (error) {
-        res.status(500).json({ message: "Server error" });
-      }
-    });
-    app.get("/validateAdminRole", async (req, res) => {
-      const userEmail = req.query.email;
-
-      try {
-        // Find the user by email in your MongoDB user collection
-        const user = await adminCollection.findOne({ email: userEmail });
-
-        if (user) {
-          res.json({ userRole: user.userRole });
-        } else {
-          res.status(404).json({ message: "User not found" });
-        }
-      } catch (error) {
-        res.status(500).json({ message: "Server error" });
-      }
-    });
-
-    // find ticket email query
-    app.get("/myTicket", async (req, res) => {
-      const email = req.query.email;
-      const query = { email: email };
-      const cursor = ticketCollection.find(query);
-      const ticket = await cursor.toArray();
-      res.send(ticket);
-    });
-    // find trip email query
-    app.get("/myTrip", async (req, res) => {
-      const email = req.query.email;
-      const query = { email: email };
-      const cursor = driverCollection.find(query);
-      const ticket = await cursor.toArray();
-      res.send(ticket);
-    });
-    // find ticket bus query
-    app.get("/busTicket", async (req, res) => {
-      const busNo = parseInt(req.query.busNo);
-
-      const query = { busNo: busNo };
-      const cursor = ticketCollection.find(query);
-      const ticket = await cursor.toArray();
-      res.send(ticket);
-    });
-    // get method for find all drivers
-    app.get("/drivers", async (req, res) => {
-      const query = {};
-      const product = await userCollection.find(query).toArray();
-      res.send(product);
-    });
-    // get method for users with email
-    app.get("/singleDrivers", async (req, res) => {
-      const email = req.query.email;
-
-      const query = { email: email };
-      const users = await userCollection.find(query).toArray();
-      res.send(users);
-    });
-    // get method for users with role query
-    app.get("/drivers", async (req, res) => {
-      const role = req.query.role;
-      const query = { role: role };
-      const users = await userCollection.find(query).toArray();
-      res.send(users);
-    });
-    // post for users method
-    app.post("/drivers", async (req, res) => {
-      const user = req.body;
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-    });
-    app.post("/admin", async (req, res) => {
-      const user = req.body;
-      const result = await adminCollection.insertOne(user);
-      res.send(result);
-    });
-
-    app.put("/myticket/:id", async (req, res) => {
-      const id = req.params.id;
-      const updateUser = req.body;
-
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          status: updateUser.status,
-          secret: updateUser.secret,
-        },
-      };
-      const result = await ticketCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
-      res.send(result);
-    });
-    app.put("/adminApprove/:id", async (req, res) => {
-      const id = req.params.id;
-      const updateUser = req.body;
-
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          status: updateUser.status,
-        },
-      };
-      const result = await managerCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
-      res.send(result);
-    });
-    //  manager part
-    // post for add confirm trip
-    app.post("/addTrip", async (req, res) => {
-      const review = req.body;
-      const result = await managerCollection.insertOne(review);
-
-      res.send(result);
-    });
-    // all the trip
-    app.get("/trips", async (req, res) => {
-      const query = {};
-      const product = await managerCollection.find(query).toArray();
-      res.send(product);
-    });
   } finally {
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Nirapode is running");
+  res.send("Bikiron Server is running");
 });
 
 httpServer.listen(PORT, () => {
